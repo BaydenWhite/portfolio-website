6ee6303e7152558e491f25f20796535b8bbbb632
BaydenWhite <baydenswhite@gmail.com>
2021-06-01 23:37:44 +1200

    Refactoring
    Refactored Footer and Tab into components

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,52 @@ import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 function App() {
   return (
     <div className="App">
-      <div className="App-header">Coming soon!</div>
+      <div className="App-header">Under construction</div>
       <div className="App-div">
-        <div className="Opaque-background">
-          <p style={{ margin: 0 }}>Welcome to my Portfolio website!</p>
-          <p>Projects:</p>
-          <a
-            className="App-link"
-            href="https://beta.civilstonks.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Civil Stonks
-          </a>
+        <OpaqueTab />
+        <div style={{ margin: 100 }}>
+          <GitHubLink />
         </div>
       </div>
-      <div id="App-footer">
-        <Button
-          variant="link"
-          className="text-light"
-          href="https://github.com/BaydenWhite?tab=repositories"
-        >
-          <FontAwesomeIcon
-            icon={faGithubSquare}
-            size="3x"
-            color="Dodgerblue"
-          ></FontAwesomeIcon>
-        </Button>
-      </div>
+      <Footer />
     </div>
   );
 }
 
+const Footer = () => {
+  return <div id="App-footer">Bayden White </div>;
+};
+
+const OpaqueTab = () => {
+  return (
+    <div className="Opaque-tab">
+      <p style={{ margin: 0 }}>Welcome to my Portfolio website!</p>
+      <p>Projects:</p>
+      <a
+        className="App-link"
+        href="https://beta.civilstonks.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Civil Stonks
+      </a>
+    </div>
+  );
+};
+
+const GitHubLink = () => {
+  return (
+    <Button
+      variant="link"
+      className="text-light"
+      href="https://github.com/BaydenWhite?tab=repositories"
+    >
+      <FontAwesomeIcon
+        icon={faGithubSquare}
+        size="3x"
+        color="Dodgerblue"
+      ></FontAwesomeIcon>
+    </Button>
+  );
+};
 export default App;
