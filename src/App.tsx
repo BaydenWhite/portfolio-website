c84e07e972a76b4eae48ff59f26097feb8e06562
BaydenWhite <baydenswhite@gmail.com>
2021-06-01 17:22:33 +1200

    Added background image and github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App-header">Coming soon!</div>
+      <div className="App-div">
+        <div className="Opaque-background">
+          <p style={{ margin: 0 }}>Welcome to my Portfolio website!</p>
+          <p>Projects:</p>
+          <a
+            className="App-link"
+            href="https://beta.civilstonks.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Civil Stonks
+          </a>
+        </div>
+      </div>
+      <div id="App-footer">
+        <Button
+          variant="link"
+          className="text-light"
+          href="https://github.com/BaydenWhite?tab=repositories"
         >
-          Learn React
-        </a>
-      </header>
+          <FontAwesomeIcon
+            icon={faGithubSquare}
+            size="3x"
+            color="Dodgerblue"
+          ></FontAwesomeIcon>
+        </Button>
+      </div>
     </div>
   );
 }
